5299c3cd9da136ad1b8b61e7c47912c7b08dadd8
Joseph A. Gerardot <indyJAG@gmail.com>
2018-01-09 10:52:24 -0500

    Add header for SimpleEngine class

---
+++ SimpleEngine.h
@@ -0,0 +1,21 @@
+#ifndef __SIMPLEENGINE__
+#include <SDL2/SDL.h>
+#include <vector>
+#include "JgTileDataEngine.h"
+
+class SimpleEngine: public JagGame::TileDataEngine {
+	public:
+	SimpleEngine();
+	const std::vector<SDL_Texture*> *getTextures(int,int);
+	void setCurrentCell(Cell);
+	void selectCell(Cell,SDL_Event*);
+	void handleKeyInput(SDL_Event*);
+	protected:
+	int mapwx;
+	int mapwy;
+	std::vector<SDL_Texture*> **textures;
+}
+
+#define __SIMPLEENGINE__
+#endif
+
